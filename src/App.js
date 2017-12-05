d4ac848a788cd7bb07bebe708e44fa91b64db63e
Brian Buchalter <bal711@gmail.com>
2017-12-05 07:31:02 -0700

    Remove create-react-app template
    We'll be adding our own styles in place of what was here.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CalendarYear from './CalendarYear.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div>
        <CalendarYear year="2017" />
        <CalendarYear year="2018" />
        <CalendarYear year="2019" />
