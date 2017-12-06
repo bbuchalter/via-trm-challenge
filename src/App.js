def128fab2cb918b53f3fc49f5ecd00ca4e776c3
Brian Buchalter <bal711@gmail.com>
2017-12-05 23:42:36 -0700

    Rework where MuiThemeProvider is added
    React was complaining about MuiThemeProvider not implementing the
    children property; let's wrap the app at a higher level to avoid this
    problem.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import MonthRangePicker from './MonthRangePicker.js';
 import AddressForm from './AddressForm.js';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
+      <div>
         <h2>Challenge 1: Month Range Picker</h2>
         <MonthRangePicker years="2017,2018,2019" />
 
         <h2>Challenge 2: Dynamic Address Form</h2>
         <AddressForm />
-      </MuiThemeProvider>
+      </div>
     );
   }
 }
